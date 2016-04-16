e7ec1bfd9a3b37d8b64f5b0a692c56d34b99bcfb
MaomiHz <maomihz@gmail.com>
2016-04-16 10:42:12 +0800

    rename window title

--- src/Gui.java
+++ src/Gui.java
@@ -1,26 +1,26 @@
-import java.util.Scanner;
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class Gui extends JFrame {
 	
 	
 	public Gui() {
-		super("LX Solver");
+		setTitle("那么问题来了");
 		setSize(300,400);
-		setLocationRelativeTo(null);
+		setLocationRelativeTo(null); // Appear on the center of screen
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		setResizable(false);
 	}
 
 	public static void main(String[] args) {
-			
-		Gui window = new Gui();
-		CheatsPanel p = new CheatsPanel();
+		Gui window = new Gui(); //Create a window
+		CheatsPanel p = new CheatsPanel(); //Create the main panel
 		window.getContentPane().add(p, BorderLayout.CENTER);
 		window.setVisible(true);
-		p.requestFocus();
+		p.requestFocus(); //Move focus to the panel
 	}
 
 }
